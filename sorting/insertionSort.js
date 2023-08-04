670d15516895fb3427e291f9e5a66a59185f255e
Sameer <sameerbagwan132@gmail.com>
2023-08-04 18:55:28 +0530

    insertionSort

--- sorting/insertionSort.js
+++ sorting/insertionSort.js
@@ -1,1 +1,18 @@
+// Big-O = O(n^2)
+
 let arrOfNum = [6, 8, 7, 80, 11, 66, 33, 9, 10];
+
+function insertion(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let insert = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > insert) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = insert;
+  }
+  return arr;
+}
+
+console.log(insertion(arrOfNum));
