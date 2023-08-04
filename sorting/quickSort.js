1ec5c4a5228f5441a774eafbc7d53d4fcd0f3277
Sameer <sameerbagwan132@gmail.com>
2023-08-05 02:07:15 +0530

    QuickSort

---
+++ sorting/quickSort.js
@@ -0,0 +1,20 @@
+let arrOfNum = [6, 8, 7, 80, 11, 66, 33, 9, 10];
+
+function quickSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  let pivot = arr[arr.length - 1];
+  let left = [];
+  let right = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+console.log(quickSort(arrOfNum));
